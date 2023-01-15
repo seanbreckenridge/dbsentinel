4ff2df6ab93e82d1188044101be6c02c94ca5c0f
Sean Breckenridge <seanbrecke@gmail.com>
2023-01-15 03:17:11 -0800

    simplify footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,36 +1,15 @@
 export default function Footer() {
   // sticky this to the bottom of the page
   return (
-    <footer className="flex flex-col items-center justify-center w-full h-24 border-t">
+    <footer className="h-30 my-5 flex w-full flex-col items-center justify-center border-t text-sm">
       <p>
-        {
-          "this site is not affiliated with MAL, database entry fields (pertaining to anime/manga enties originally from MAL) is property of MAL, see "
-        }
-        <a
-          href="https://myanimelist.net/membership/terms_of_use"
-          className="text-blue-400 hover:text-blue-300"
-        >
-          {"terms of use"}
-        </a>
-      </p>
-      <p>
-        {"all other connected data is released under "}
-        <a
-          href="https://creativecommons.org/share-your-work/public-domain/cc0/"
-          className="text-blue-400 hover:text-blue-300"
-        >
-          {"CC0"}
-        </a>
-      </p>
-      <p>
-        {"source code is released under the MIT license "}
+        {`${new Date().getFullYear()} © malsentinel `}
         <a
           className="text-blue-400 hover:text-blue-300"
           href="https://github.com/seanbreckenridge/malsentinel"
         >
-          here
+          source code
         </a>
-        .
       </p>
     </footer>
   );
