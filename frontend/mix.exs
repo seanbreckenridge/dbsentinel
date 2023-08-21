c6fd5d74dcdf9fb923d1f29d4dfb430e94acd0be
Sean Breckenridge <seanbrecke@gmail.com>
2023-08-21 01:29:51 -0700

    update mix aliases

--- frontend/mix.exs
+++ frontend/mix.exs
@@ -71,7 +71,8 @@ defmodule Frontend.MixProject do
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind default", "esbuild default"],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"],
+      "prod.setup": ["ecto.create", "ecto.migrate", "phx.digest.clean"]
     ]
   end
 end
